49ecd25c713e3c74189a6828d1a84224f5993742
baharmohimani <br.1990m@gmail.com>
2016-10-26 14:31:47 -0700

    fixing the server

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.sendfile('Bahar.html');
 });
 
+http.listen(3000, function(){
+ console.log('listening on *:3000');
+ });
+
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on', http.address().port);
 });
