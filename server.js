d9bb4f43491969e3066e3c3070fd3002ff5dfa65
baharmohimani <br.1990m@gmail.com>
2016-10-26 14:29:26 -0700

    update the server

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.sendfile('Bahar.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
 });
